806215a9a13ec8f674a3ad5d82b5a450b41d5399
Javier <tuya@uniovi.es>
2022-11-27 20:51:28 +0100

    Combined dependency updates (2022-11-27) (#180)
    Combined dependency updates (2022-11-27)
    
    Includes these updates:
    - [Pull Request for dependabot/update/i0](https://github.com/javiertuya/test-qabot/pull/176)
    - [Pull Request for dependabot/update/i2](https://github.com/javiertuya/test-qabot/pull/178)
    
    Does not include these updates because of merge conflicts:
    - [Pull Request for dependabot/update/i1](https://github.com/javiertuya/test-qabot/pull/177)
    - [Pull Request for dependabot/update/i3](https://github.com/javiertuya/test-qabot/pull/179)

--- main-script.sh
+++ main-script.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # for qabot pull request testing
-echo "this is line0pass"
+echo "this is line0fail"
 echo "this is line1pass"
-echo "this is line2pass"
+echo "this is line2fail"
 echo "this is line3pass"
 echo "ending"
 exit 0
