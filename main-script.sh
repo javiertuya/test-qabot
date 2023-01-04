0afcc580d5fe1e6622e4acb2425971fcf84c44c6
root <root@jenkins-slave-xb>
2023-01-04 22:59:38 +0100

    Set change at branch dependabot/testupdate/single-20230104-225929-1

--- main-script.sh
+++ main-script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # for qabot pull request testing
 echo "this is line0pass"
-echo "this is line1pass"
+echo "this is line1fail"
 echo "this is line2pass"
 echo "this is line3pass"
 echo "ending"
